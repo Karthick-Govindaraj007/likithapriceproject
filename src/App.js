460c3da85489412db1d464779a14502cda062781
Karthick Govindaraj <136472779+Karthick-Govindaraj007@users.noreply.github.com>
2025-01-11 13:49:17 -0500

    deleted the random not functional copy files imported needed modules site is functional locally now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LevelsPage from "./components/LevelsPage";
+import ElevatorPage from "./components/ElevatorPage";
+import GreatMigration from "./components/GreatMigration";
+import Socialism from "./components/Socialism";
+import WomenProhibition from "./components/WomenProhibition";
+import RacialIntolerance from "./components/RacialIntolerance";
+import SocialIntolerance from "./components/SocialIntolerance";
+import WorkersRights from "./components/WorkersRights";
+import HomePage from "./components/HomePage";
+import EssentialQuestions from "./components/EssentialQuestions.js";
+import Theatre from "./components/Theatre";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [currentFloor, setCurrentFloor] = useState(1); // Track current floor
+
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<HomePage />}
+                />
+                <Route path="/eqs" element={<EssentialQuestions />} />
+                <Route
+                    path="/levels"
+                    element={<LevelsPage currentFloor={currentFloor} setCurrentFloor={setCurrentFloor} />}
+                />
+                <Route
+                    path="/elevator"
+                    element={<ElevatorPage setCurrentFloor={setCurrentFloor} />}
+                />
+                <Route path="/greatmigration" element={<GreatMigration />} />
+                <Route path="/socialism" element={<Socialism />} />
+                <Route path="/women" element={<WomenProhibition />} />
+                <Route path="/racialintolerance" element={<RacialIntolerance />} />
+                <Route path="/socialintolerance" element={<SocialIntolerance />} />
+                <Route path="/workersrights" element={<WorkersRights />} />
+                <Route path="/theatre" element={<Theatre />} />
+
+
+
+
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
